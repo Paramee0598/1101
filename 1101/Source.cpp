9144b8908e2015c46629cfec899baed33cd1c89b
Paramee Chumsri <69068301+Paramee0598@users.noreply.github.com>
2020-09-13 16:33:05 +0700

    Update Source.cpp

--- 1101/Source.cpp
+++ 1101/Source.cpp
@@ -3,11 +3,14 @@ int main() {
     int i, j, rows;
     printf("Enter the number of rows: ");
     scanf_s("%d", &rows);
+    if (rows >= 0 ){
     for (i = 1; i <= rows; ++i) {
         for (j = 1; j <= i; ++j) {
             printf("%d ", j);
-        }
+            }
         printf("\n");
+        }
     }
+    else printf("Error ");
     return 0;
 }
